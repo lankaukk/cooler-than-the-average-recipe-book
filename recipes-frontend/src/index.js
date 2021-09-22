4dc6dbcbf3ae6497575f676c9612da265e1c6485
McKayla Lankau <lankaukk@yahoo.com>
2021-09-22 16:18:34 -0400

    set up index.js file and created store

--- recipes-frontend/src/index.js
+++ recipes-frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
@@ -8,5 +8,14 @@ import App from './App';
 
 // set up our store
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , 
+    document.getElementById('root'));
 
