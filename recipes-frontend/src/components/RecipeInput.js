893ba3b7b1ebcf3035d1243536b3a8b9c130703e
McKayla Lankau <lankaukk@yahoo.com>
2021-09-23 14:02:52 -0400

    clears new recipe inputs after submit, because it is a controlled form

--- recipes-frontend/src/components/RecipeInput.js
+++ recipes-frontend/src/components/RecipeInput.js
@@ -18,6 +18,10 @@ class RecipeInput extends React.Component {
     handleSubmit = (event) => {
         event.preventDefault()
         this.props.addRecipe(this.state)
+        this.setState({
+            name: '',
+            description: ''
+        })
     }
 
     render() {
