205d362edb5573d7b4b4fbf8313d45174ffaa427
McKayla Lankau <lankaukk@yahoo.com>
2021-10-14 15:56:08 -0700

    fixed fetching error due to cors file

--- recipes-backend/config/initializers/cors.rb
+++ recipes-backend/config/initializers/cors.rb
@@ -7,12 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins '*'
-    origins 'http://localhost:3000'
+    origins '*'
+    # origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
