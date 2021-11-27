b0d9a13f90f289791bd4c163e8cf34160a94fa07
McKayla Lankau <lankaukk@yahoo.com>
2021-11-27 13:27:59 -0500

    Update fetchRecipes.js

--- recipes-frontend/src/actions/fetchRecipes.js
+++ recipes-frontend/src/actions/fetchRecipes.js
@@ -1,5 +1,4 @@
 export function fetchRecipes() {
-//console.log("inside fetchRecipes")
     return (dispatch) => {
         fetch('http://localhost:3000/api/v1/recipes')
             .then(response => response.json())
